ff3b5e9eb8fe5e599235980f643a67f429312d3d
mustafa2smi-ui <mustafa2smi@gmail.com>
2026-01-10 09:46:28 +0530

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'khata-v1';
+const assets = [
+  '/',
+  '/index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});

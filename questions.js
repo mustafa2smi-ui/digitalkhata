6b9aefe03586960b9f5fb343af63f919dafaf4a0
mustafa2smi-ui <mustafa2smi@gmail.com>
2026-03-21 11:58:33 +0530

    Update and rename levels.js to questions.js

---
+++ questions.js
@@ -0,0 +1,25 @@
+const gameData = {
+    "Science": [
+        {
+            level: 1,
+            pairs: [
+                { q: "🌡️", sub: "Temperature", a: "Thermometer" },
+                { q: "🔬", sub: "Small Cells", a: "Microscope" },
+                { q: "🧪", sub: "Chemicals", a: "Test Tube" },
+                { q: "☀️", sub: "Solar Star", a: "Sun" },
+                { q: "🌍", sub: "Our Planet", a: "Earth" }
+            ]
+        }
+    ],
+    "Technology": [
+        {
+            level: 1,
+            pairs: [
+                { q: "💻", sub: "Coding", a: "Laptop" },
+                { q: "📱", sub: "Calling", a: "Mobile" },
+                // Image Example: Agar aapke paas 'mouse.png' file hai
+                { q: "mouse.png", sub: "Pointer", a: "Mouse" } 
+            ]
+        }
+    ]
+};
